3745c963e760bd9859d524753fe81b195ab9928e
VachhaniNirav <99639408+VachhaniNirav@users.noreply.github.com>
2022-08-18 20:32:00 +0530

    updated meta tags

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -139,6 +139,7 @@ const CoffeeStore = (initialProps) => {
         <div className={styles.layout}>
             <Head>
                 <title>{name}</title>
+                <meta name="Discover your local coffee shops!" content={`${name} coffee store`} />
             </Head>
             <div className={styles.container}>
                 <div className={styles.col1}>
